8a969fd46bc251d853a27aadc9f3b24ccbb27d4e
Jona <josuecondori2002@gmail.com>
2023-11-06 17:23:36 -0500

    Configurande url Backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://task-manager-api-f3rk.onrender.com",
     withCredentials: true,
 });
 
