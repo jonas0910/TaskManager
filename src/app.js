e3bf66df12710e541992ec2d70e06df5cf71c33b
Jona <josuecondori2002@gmail.com>
2023-11-06 17:41:43 -0500

    Configurando cors 3

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import cors from "cors";
 const app=express();
 
 app.use(cors({
-    origin:"https://task-manager-front-611p.onrender.com/",
+    origin:"https://task-manager-front-611p.onrender.com",
     credentials:true,
 }));
 app.use(express.json());
