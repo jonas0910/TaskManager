6b9e5002867a121a14e289b2d0c534bff5d742b5
Jona <josuecondori2002@gmail.com>
2023-11-06 19:40:44 -0500

    Configuraciones 5

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ export const connectDB = async() => {
         await mongoose.connect('mongodb+srv://jjcondoric:j1S6GcoWIucCtTH7@task-manager-db.hnr10tq.mongodb.net/Task-Manager-DB?retryWrites=true&w=majority',{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
+            useFindAndModify: true,
             useCreateIndex: true
         });
         console.log('>>>DB is connected');
