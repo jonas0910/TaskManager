6ef6e2f7f23c75e15d3dfd48b9b7ebfa393cdb05
Jona <josuecondori2002@gmail.com>
2023-11-06 19:18:34 -0500

    Configuraciones 2

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect('mongodb+srv://jjcondoric:j1S6GcoWIucCtTH7@task-manager-db.hnr10tq.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://jjcondoric:j1S6GcoWIucCtTH7@task-manager-db.hnr10tq.mongodb.net/Task-Manager-DB?retryWrites=true&w=majority');
         console.log('>>>DB is connected');
     } catch (error) {
         console.log(error);
